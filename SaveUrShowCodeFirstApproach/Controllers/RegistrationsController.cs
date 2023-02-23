d146017b0fb8b9ba1d6b7af4528dae8eaad1097c
Sowmya <sowmyap1881@gmail.com>
2023-02-23 15:55:50 +0530

    Add project files.

---
+++ SaveUrShowCodeFirstApproach/Controllers/RegistrationsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SaveUrShowCodeFirstApproach.Models;
+
+namespace SaveUrShowCodeFirstApproach.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationsController : ControllerBase
+    {
+        private readonly SaveUrShowCFADbContext _context;
+
+        public RegistrationsController(SaveUrShowCFADbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Registrations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Registration>>> GetRegistration()
+        {
+            return await _context.Registration.ToListAsync();
+        }
+
+        [HttpGet("{email}/{password}")]
+        public async Task<ActionResult<Registration>> GetRegistration(string email,string password)
+        {
+            var registration = await _context.Registration.FirstOrDefaultAsync(record=>record.email==email && record.password==password); //using lambda function we r checking the email and password of a register user
+
+            if (registration == null)
+            {
+                return NotFound();
+            }
+
+            return registration;
+        }
+
+        // GET: api/Registrations/5
+        /*
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Registration>> GetRegistration(int id)
+        {
+            var registration = await _context.Registration.FindAsync(id);
+
+            if (registration == null)
+            {
+                return NotFound();
+            }
+
+            return registration;
+        }
+        */
+
+        // PUT: api/Registrations/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRegistration(int id, Registration registration)
+        {
+            if (id != registration.userid)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(registration).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RegistrationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Registrations
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Registration>> PostRegistration(Registration registration)
+        {
+            _context.Registration.Add(registration);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRegistration", new { id = registration.userid }, registration);
+        }
+
+        // DELETE: api/Registrations/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Registration>> DeleteRegistration(int id)
+        {
+            var registration = await _context.Registration.FindAsync(id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+
+            _context.Registration.Remove(registration);
+            await _context.SaveChangesAsync();
+
+            return registration;
+        }
+
+        private bool RegistrationExists(int id)
+        {
+            return _context.Registration.Any(e => e.userid == id);
+        }
+    }
+}
---
+++ SaveUrShowCodeFirstApproach/Models/SaveUrShowCFADbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaveUrShowCodeFirstApproach.Models
+{
+    public class SaveUrShowCFADbContext : DbContext
+    {
+        public SaveUrShowCFADbContext(DbContextOptions<SaveUrShowCFADbContext> options)
+          : base(options)
+        {
+        }
+        public virtual DbSet<Registration> Registration { get; set; }
+        public virtual DbSet<FindTicket> FindTicket { get; set; }
+        public virtual DbSet<BookTicket> BookTicket { get; set; }
+
+    }
+}
